bf67407670b2eaa967f4400ffd647a7875e0966e
ekko <152005280+EKKOLearnAI@users.noreply.github.com>
2026-06-20 20:23:56 +0800

    Bump Web UI to 0.6.18 (#1696)

--- packages/client/src/data/changelog.ts
+++ packages/client/src/data/changelog.ts
@@ -6,6 +6,17 @@ export interface ChangelogEntry {
 
 export const changelog: ChangelogEntry[] = [
   {
+    version: '0.6.18',
+    date: '2026-06-20',
+    changes: [
+      'changelog.new_0_6_18_2',
+      'changelog.new_0_6_18_3',
+      'changelog.new_0_6_18_4',
+      'changelog.new_0_6_18_5',
+      'changelog.new_0_6_18_6',
+    ],
+  },
+  {
     version: '0.6.17',
     date: '2026-06-17',
     changes: [
@@ -42,70 +53,6 @@ export const changelog: ChangelogEntry[] = [
       'changelog.new_0_6_15_8',
       'changelog.new_0_6_15_9',
       'changelog.new_0_6_15_10',
-    ],
-  },
-  {
-    version: '0.6.14',
-    date: '2026-06-11',
-    changes: [
-      'changelog.new_0_6_14_1',
-      'changelog.new_0_6_14_2',
-      'changelog.new_0_6_14_3',
-      'changelog.new_0_6_14_4',
-      'changelog.new_0_6_14_5',
-      'changelog.new_0_6_14_6',
-      'changelog.new_0_6_14_7',
-      'changelog.new_0_6_14_8',
-      'changelog.new_0_6_14_9',
-      'changelog.new_0_6_14_10',
-    ],
-  },
-  {
-    version: '0.6.12',
-    date: '2026-06-09',
-    changes: [
-      'changelog.new_0_6_12_1',
-      'changelog.new_0_6_12_2',
-      'changelog.new_0_6_12_3',
-      'changelog.new_0_6_12_4',
-      'changelog.new_0_6_12_5',
-      'changelog.new_0_6_12_6',
-      'changelog.new_0_6_12_7',
-      'changelog.new_0_6_12_8',
-      'changelog.new_0_6_12_9',
-      'changelog.new_0_6_12_10',
-    ],
-  },
-  {
-    version: '0.6.11',
-    date: '2026-06-06',
-    changes: [
-      'changelog.new_0_6_11_1',
-      'changelog.new_0_6_11_2',
-      'changelog.new_0_6_11_3',
-      'changelog.new_0_6_11_4',
-      'changelog.new_0_6_11_5',
-      'changelog.new_0_6_11_6',
-      'changelog.new_0_6_11_7',
-      'changelog.new_0_6_11_8',
-      'changelog.new_0_6_11_9',
-      'changelog.new_0_6_11_10',
-    ],
-  },
-  {
-    version: '0.6.10',
-    date: '2026-06-03',
-    changes: [
-      'changelog.new_0_6_10_1',
-      'changelog.new_0_6_10_2',
-      'changelog.new_0_6_10_3',
-      'changelog.new_0_6_10_4',
-      'changelog.new_0_6_10_5',
-      'changelog.new_0_6_10_6',
-      'changelog.new_0_6_10_7',
-      'changelog.new_0_6_10_8',
-      'changelog.new_0_6_10_9',
-      'changelog.new_0_6_10_10',
     ],
   },
 ]
